e2088ad519d1acf9ecbb8d4dc1cee2242657c0cc
Gaurang <gaurangpansare@yahoo.in>
2017-06-17 19:45:53 +0530

    works just as it should

--- proper_octagon_coordinates.py
+++ proper_octagon_coordinates.py
@@ -1,11 +1,11 @@
 from math import *
 import numpy
 
-sides = 90
+side = 121
 n = 8
-nLevels = sides / (n + 1)
-org = - sides * cos(pi / 8)
-print nLevels
+nLevels = 11
+org = - side * cos(pi / 8)
+#print nLevels
 x, y = numpy.zeros((nLevels, n)), numpy.zeros((nLevels, n))
 j = 0
 
@@ -13,7 +13,7 @@ def intOf(floatVal):
     return int(round(floatVal))
 
 
-for i in range(sides/(n + 1), sides + 1, sides/(n + 1)):
+for i in range(side/nLevels, side + 1, side/nLevels):
     #print "i value", i
     #two points in one quadrant of an octagon
     xl = i * cos(pi / 8)
@@ -40,15 +40,30 @@ for i in range(sides/(n + 1), sides + 1, sides/(n + 1)):
 
     print intOf(xl - org), intOf(yl - org), intOf(xh - org), intOf(yh - org), intOf(-xh - org), intOf(yh - org), intOf(-xl - org), intOf(yl - org), intOf(-xl - org), intOf(-yl - org), intOf(-xh - org), intOf(-yh - org), intOf(xh - org), intOf(-yh - org), intOf(xl - org), intOf(-yl - org)
     j+=1
-print y
-while(j != 1):
-    for i in range(8):
-        #print '<a xlink:href="#"><polygon class="teal" points="', x[j][i], y[j][i], '" /></a>'
-        a = 7
-for i in range(nLevels):
-    for j in range(n - 1):
-        tx1, ty1 = intOf(x[i][j]), intOf(y[i][j])
-        tx2, ty2 = intOf(x[i][j + 1]), intOf(y[i][j + 1])
-        tx3, ty3 = intOf(x[i + 1][j]), intOf(y[i + 1][j])
-        tx4, ty4 = intOf(x[i + 1][j + 1]), intOf(y[i + 1][j + 1])
-        #print '<a xlink:href="#"><polygon class="teal" points="', tx1, ty1, tx2, ty2, tx3, ty3, tx4, ty4, '" /></a>'
+
+i = 7
+while i > 0:
+    for j in range(nLevels - 1):
+        tx1 = x[j][i]
+        tx4 = x[j][i - 1]
+        tx2 = x[j+1][i]
+        tx3 = x[j+1][i-1]
+        ty1 = y[j][i]
+        ty4 = y[j][i - 1]
+        ty2 = y[j+1][i]
+        ty3 = y[j+1][i-1]
+        print '<a xlink:href="#"><polygon class="slice-' + str(i) + '" points="', tx1, ty1, tx2, ty2, tx3, ty3, tx4, ty4, '" /></a>'
+
+        
+    i-=1
+
+for j in range(nLevels - 1):
+    tx1 = x[j][0]
+    tx4 = x[j][7]
+    tx2 = x[j+1][0]
+    tx3 = x[j+1][7]
+    ty1 = y[j][0]
+    ty4 = y[j][7]
+    ty2 = y[j+1][0]
+    ty3 = y[j+1][7]
+    print '<a xlink:href="#"><polygon class="slice-0" points="', tx1, ty1, tx2, ty2, tx3, ty3, tx4, ty4, '" /></a>'
